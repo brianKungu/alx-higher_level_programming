7d399cb4d786db7778c9c0c93482a31724f7e3d3
brianKungu <bkungu07@gmail.com>
2022-08-11 15:15:51 +0300

    SQL2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,8 +1,4 @@
 -- script that creates database hbtn_0c_0
 -- does not fail
 
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbtn_0c_0')
-BEGIN
-  CREATE DATABASE hbtn_0c_0;
-END;
-GO
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

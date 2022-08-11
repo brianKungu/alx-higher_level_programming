6cbbb222deb4f74346dbec95ecb5107ac0d6ae13
brianKungu <bkungu07@gmail.com>
2022-08-11 16:00:15 +0300

    8&9

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a new row in table first_table
 -- in database hbtn_0c_0
-INSERT INTO hbtn_0c_0 (id,name) VALUES(89,"Best School");
+INSERT INTO first_table (id,name) VALUES(89,"Best School");

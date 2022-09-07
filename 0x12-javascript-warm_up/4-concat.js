b02b5142ef57e49d21d6e9b8cadaa2bb86e480bc
brianKungu <bkungu07@gmail.com>
2022-09-07 21:48:20 +0300

    warm_up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log(process.argv[2] + " is " + process.argv[3]);
+
+// Script that prints "JavaScript is amazing".
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);

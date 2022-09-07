b7472f4412b343c33f8dbbf8948c0fe065d3ba08
brianKungu <bkungu07@gmail.com>
2022-09-08 00:47:38 +0300

    1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,32 @@
 // script that finds the second biggest integer in list of arguments passed to it.
 
 if (process.argv.length < 4) {
-  console.log('0');
+  console.log("0");
 } else {
   const args = [];
 
   for (let i = 2; i < process.argv.length; i++) {
     args[i - 2] = process.argv[i];
   }
-  args.sort(function (a, b) { return b - a; });
+  args.sort(function (a, b) {
+    return b - a;
+  });
   console.log(args[1]);
 }
+
+// function myFunction(a) {
+//   console.log(a);
+// }
+
+// const number = 12;
+// myFunction(number);
+
+// let b = 1;
+
+// function myFunction(a) {
+//     console.log(a + b);
+//     b = a;
+// }
+// myFunction(3);
+
+// myFunction(4);

ea666d26461024fbd3959f98f7ee40b77045cf18
brianKungu <bkungu07@gmail.com>
2022-09-07 22:43:48 +0300

    10.1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (n < 0) {
-    return -1;
-  }
-  if (n === 0 || isNaN(n)) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
-
-console.log(factorial(Number(process.argv[2])));
-
-// function Factorial(num) {
-//   var factors;
-//   if (num === 0) {
-//     return 1;
-//   } else {
-//     return num * Factorial(num - 1);
+// function factorial(n) {
+//   if (n < 0) {
+//     return -1;
 //   }
+//   if (n === 0 || isNaN(n)) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
 // }
 
-// console.log(Factorial(10));
+// console.log(factorial(Number(process.argv[2])));
+
+function Factorial(num) {
+  var factors;
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * Factorial(num - 1);
+  }
+}
+
+console.log(Factorial(10));

9ae19523f46c0510e8126fa32c9d21a9e0016ea9
brianKungu <bkungu07@gmail.com>
2022-09-07 22:42:06 +0300

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,23 @@
 #!/usr/bin/node
-
-// Script to find the factorial of a number
-
-function factorial (num) {
-  if (num === 0 || isNaN(num)) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
+function factorial(n) {
+  if (n < 0) {
+    return -1;
   }
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
+
+// function Factorial(num) {
+//   var factors;
+//   if (num === 0) {
+//     return 1;
+//   } else {
+//     return num * Factorial(num - 1);
+//   }
+// }
+
+// console.log(Factorial(10));
